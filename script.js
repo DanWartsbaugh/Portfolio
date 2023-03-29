7e27f683f1d27739b1365ffb9661b920999a7fa3
DanWartsbaugh <d.wartsbaugh@gmail.com>
2023-03-29 17:15:02 -0500

    second day of progress

---
+++ script.js
@@ -0,0 +1,9 @@
+// var a=document.querySelector(.links a);
+
+function changeColor(element){
+    element.style.color = "red";
+}
+
+function changeBack(element){
+    element.style.color = "black";
+}
