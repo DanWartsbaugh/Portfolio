d6d8666fbb27c424c6f8eb949764a5989f1dbb4b
DanWartsbaugh <d.wartsbaugh@gmail.com>
2023-03-30 17:20:18 -0500

    added notes, changed color scheme, cleaned up

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// var a=document.querySelector(.links a);
-
+//Change the color of links on hover
 function changeColor(element){
-    element.style.color = "red";
+    element.style.color = "rgb(101,126,130)";
 }
 
+//Change the color of links back to original on mouseout
 function changeBack(element){
     element.style.color = "black";
 }
